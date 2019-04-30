9310e6415e0a47e3568e2950b3c11283d6c58d19
Daniel Khan <daniel@khan.io>
2019-04-30 13:31:34 +0200

    remove dist

--- dist/Indicator.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Indicator {
-}
-exports.Indicator = Indicator;
-//# sourceMappingURL=Indicator.js.map
--- dist/Source.d.ts
+++
@@ -1,8 +0,0 @@
-import * as pitometer from 'pitometer';
-export declare class Source implements pitometer.ISource {
-    private queryUrl;
-    constructor({ queryUrl }: {
-        queryUrl: any;
-    });
-    fetch(query: any): Promise<any>;
-}
